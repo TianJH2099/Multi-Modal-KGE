867cb68d983d8380d663aa85e938802d818615a1
TianJH <1328560238@qq.com>
2022-09-26 18:42:55 +0800

    update

--- dataset.py
+++ dataset.py
@@ -2,19 +2,14 @@ from torch.utils.data import Dataset
 import os
 
 class TripleSets(Dataset):
-    def __init__(self, path='../input/mkgsets'):
-        sets = ['train', 'valid']
-        path1 = os.path.join(path, '{}2id.txt'.format(sets[0]))
-        path2 = os.path.join(path, '{}2id.txt'.format(sets[1]))
-        f1 = open(path1, 'r')
-        f2 = open(path2, 'r')
-        data1 = f1.readlines()[1:]
-        data1 = [line.split() for line in data1]
-        data1 = [[int(num) for num in line] for line in data1]
-        data2 = f2.readlines()[1:]
-        data2 = [line.split() for line in data2]
-        data2 = [[int(num) for num in line] for line in data2]
-        self.data = data1 + data2
+    def __init__(self, path='OpenBG-IMG', c="train"):
+
+        f_path = os.path.join(path, '{}2id.tsv'.format(c))
+        with open(f_path, 'r') as f:
+            data = f.readlines()[0:]
+            data = [line.split() for line in data]
+            data = [[int(num) for num in line] for line in data]
+        self.data = data
     
     def __getitem__(self, index):
         item = self.data[index]
@@ -22,3 +17,8 @@ class TripleSets(Dataset):
     
     def __len__(self):
         return len(self.data)
+
+if __name__=="__main__":
+    mySet = TripleSets()
+    print(mySet.__getitem__(1))
+    print(mySet.__len__())
