c878612b7028565406767b110e1150752dd51cec
TianJH <1328560238@qq.com>
2022-10-02 17:31:41 +0800

    modify evaluate function

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,8 @@
 import numpy as np
+
 np.set_printoptions(precision=3)
 
-def evaluate(pre, target, emb, device):
+def evaluate(pre, target, device):
     # pre [batch size, embeding dim]
     # target [batch size]
     # emb [num embedings, embeding size]
@@ -10,22 +11,24 @@ def evaluate(pre, target, emb, device):
     tot_hit10 = 0
     tot_MRR = 0.0
     tot_MR = 0
+#     pre = emb.unsqueeze(1) - pre.unsqueeze(0)  # pre = [num embedings,batch size, embeding dim]
+#     pre = pre.transpose(0, 1)  # pre = [batch size, num embedings, embeding dim]
+#     temp = torch.ones(pre.shape[0], pre.shape[2], 1) # tmep = [batch size, embeding dim, 1]
+#     res = torch.bmm(pre.to(device), temp.to(device)) # res = [batch size, num embedings, 1]
+#     res = res.reshape(res.shape[0], res.shape[1]) # res = [batch size, num embedings]
 
+#     pre = (-res).cpu().detach().numpy()
     pre = pre.cpu().detach().numpy()
     for i,each in enumerate(pre):
-        max_idx = np.argmax(each)
-        m = np.argpartition(each, max_idx)
-        if target[i] == max_idx:
+        t = each[target[i]]
+        each.sort()
+        t_idx = np.where(each==t)[0][0]
+        if t_idx < 1:
             tot_hit1 += 1
-        if target[i] in m[:3]:
+        if t_idx < 3:
             tot_hit3 += 1
-        if target[i] in m[:10]:
+        if t_idx < 10:
             tot_hit10 += 1
-            
-        for j, idx in enumerate(m):
-            if(idx==target[i]):
-                tot_MRR += 1.0/(j+1.0)
-                tot_MR += j+1
-                break
-    
+        tot_MRR += 1.0/(t_idx+1)
+        tot_MR += t_idx+1
     return tot_MRR, tot_MR, tot_hit1, tot_hit3, tot_hit10
