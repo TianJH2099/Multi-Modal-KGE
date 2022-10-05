6d5f7771428b89915267dddb2540a46edfde3678
TianJH <1328560238@qq.com>
2022-10-05 18:23:16 +0800

    update

--- evaluate.py
+++ evaluate.py
@@ -1,28 +1,24 @@
 import numpy as np
+import torch
 
 np.set_printoptions(precision=3)
 
 def evaluate(pre, target, device):
     # pre [batch size, embeding dim]
     # target [batch size]
-    # emb [num embedings, embeding size]
     tot_hit1 = 0
     tot_hit3 = 0
     tot_hit10 = 0
     tot_MRR = 0.0
     tot_MR = 0
-#     pre = emb.unsqueeze(1) - pre.unsqueeze(0)  # pre = [num embedings,batch size, embeding dim]
-#     pre = pre.transpose(0, 1)  # pre = [batch size, num embedings, embeding dim]
-#     temp = torch.ones(pre.shape[0], pre.shape[2], 1) # tmep = [batch size, embeding dim, 1]
-#     res = torch.bmm(pre.to(device), temp.to(device)) # res = [batch size, num embedings, 1]
-#     res = res.reshape(res.shape[0], res.shape[1]) # res = [batch size, num embedings]
 
-#     pre = (-res).cpu().detach().numpy()
-    pre = pre.cpu().detach().numpy()
+    # pre = -pre.cpu().detach().numpy()
     for i,each in enumerate(pre):
-        t = each[target[i]]
-        each.sort()
-        t_idx = np.where(each==t)[0][0]
+        t = target[i]
+        sort_value, sort_key = torch.sort(each, dim=0, descending=True)
+        sort_key = sort_key.cpu().numpy()
+        # each.sort(descending=True)
+        t_idx = np.where(sort_key==t)[0][0]
         if t_idx < 1:
             tot_hit1 += 1
         if t_idx < 3:
