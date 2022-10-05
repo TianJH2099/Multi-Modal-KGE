ded1e9ce3cb38d33c54647b67bf1c3cac926fe9b
TianJH <1328560238@qq.com>
2022-10-05 19:28:01 +0800

    use pandas to read files

--- dataset.py
+++ dataset.py
@@ -1,18 +1,16 @@
 from torch.utils.data import Dataset
 import os
+import pandas as pd
 
 class TripleSets(Dataset):
     def __init__(self, path='OpenBG-IMG', c="train"):
 
         f_path = os.path.join(path, '{}2id.tsv'.format(c))
-        with open(f_path, 'r') as f:
-            data = f.readlines()[0:]
-            data = [line.split() for line in data]
-            data = [[int(num) for num in line] for line in data]
-        self.data = data
+        f = pd.read_csv(f_path, sep='\t')
+        self.data = f
     
     def __getitem__(self, index):
-        item = self.data[index]
+        item = self.data.loc[index]
         return item[0], item[2], item[1]
     
     def __len__(self):
